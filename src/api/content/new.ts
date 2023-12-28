877a2215e53b3901308e6ee1c841cd7302a3c677
liangjinhai <fateking27@outlook.com>
2023-12-28 16:09:41 +0800

    feat: news-cover

--- src/api/content/new.ts
+++ src/api/content/new.ts
@@ -8,6 +8,13 @@ export const listNew = (data?: object) => {
   });
 };
 
+/**查询新闻封面**/
+export const NewImg = (data?: object) => {
+  return http.request<Result>("get", "/wjx/material/tree", {
+    params: data
+  });
+};
+
 /**id查询新闻标题**/
 export const getNew = id => {
   return http.request<Result>("get", `/business/news_info/${id}`);
