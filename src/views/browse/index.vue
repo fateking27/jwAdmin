7b95ba3095d2b002cf88ebe072436ad6890140bb
lyancong <2369036293@qq.com>
2023-12-29 16:52:31 +0800

    feat: statistics

--- src/views/browse/index.vue
+++ src/views/browse/index.vue
@@ -67,6 +67,11 @@
 
       <el-tab-pane label="图表">
         <div id="chart" ref="chart" style="width: 600px; height: 400px" />
+        <div
+          id="titleChart"
+          ref="titleChart"
+          style="width: 600px; height: 400px"
+        />
       </el-tab-pane>
     </el-tabs>
     <Form ref="formRef" @reload="onSearch" />
@@ -208,6 +213,7 @@ async function onSearch() {
   //获取近一个月的新闻信息
   const { rows } = await listArticle();
   renderChart();
+  titleChart();
   dataList.value = rows;
   loading.value = false;
   console.log(rows);
@@ -345,6 +351,76 @@ function renderChart() {
     }
   });
   option && myChart.setOption(option);
+}
+
+function titleChart() {
+  const chart = document.getElementById("titleChart");
+  const titleChart = echarts.init(chart);
+
+  const options = {
+    title: {
+      text: "Stacked Line"
+    },
+    tooltip: {
+      trigger: "axis"
+    },
+    legend: {
+      data: ["Email", "Union Ads", "Video Ads", "Direct", "Search Engine"]
+    },
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: "category",
+      boundaryGap: false,
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    },
+    yAxis: {
+      type: "value"
+    },
+    series: [
+      {
+        name: "Email",
+        type: "line",
+        stack: "Total",
+        data: [120, 132, 101, 134, 90, 230, 210]
+      },
+      {
+        name: "Union Ads",
+        type: "line",
+        stack: "Total",
+        data: [220, 182, 191, 234, 290, 330, 310]
+      },
+      {
+        name: "Video Ads",
+        type: "line",
+        stack: "Total",
+        data: [150, 232, 201, 154, 190, 330, 410]
+      },
+      {
+        name: "Direct",
+        type: "line",
+        stack: "Total",
+        data: [320, 332, 301, 334, 390, 330, 320]
+      },
+      {
+        name: "Search Engine",
+        type: "line",
+        stack: "Total",
+        data: [820, 932, 901, 934, 1290, 1330, 1320]
+      }
+    ]
+  };
+
+  options && titleChart.setOption(options);
 }
 
 onMounted(() => {
